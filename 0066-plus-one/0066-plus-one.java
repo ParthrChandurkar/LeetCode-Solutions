e44b9fa58a2952048ee5a5875cd7bd7d0c6160e3
Parth Chandurkar <parthrchn27@gmail.com>
2026-08-04 18:02:56 +0530

    Time: 0 ms (100%), Space: 43.5 MB (43.44%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] plusOne(int[] given) {
+
+       int m = given.length;
+
+        for(int i = m-1 ; i>=0 ; i--){
+            if(given[i] != 9 ){
+                given[i]++;
+
+                return given; 
+            }
+            else{
+                given[i] = 0 ;
+                
+            }
+        }
+
+        int[] ans = new int[m + 1];
+        ans[0] = 1;
+        
+        return ans;
+    }
+}
